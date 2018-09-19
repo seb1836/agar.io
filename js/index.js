da621d48504d71ba5a7c6bafbcfb1baff6839be4
Tristan Moreau <trst.moreau@gmail.com>
2018-09-19 21:17:05 +0200

    added steps 4-5-6-7-8

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// TODO : rename 
 var circle = {
   name: "fsdfs",
   color: "red",
@@ -7,8 +8,21 @@ var circle = {
   }
 };
 
-//step 1
+// TODO : create an array of circles
+// Note : all circles (and the player) should have the same CSS class with no dimension, the dimensions are handled by the JS
 
+// step 1
+
+// TODO : create a new init function to init the playzone
+// with all the circles and the player
+// Note : don't forget to call the function 
+var initialize = function() {
+  // initialize player
+  // initialize circles => randomly if you want (bonus)
+}
+
+// TODO : change the name of this function
+// use a better naming
 var init = function() {
   document.getElementsByClassName(
     "chosenName"
@@ -19,13 +33,15 @@ var init = function() {
 };
 
 document.getElementById("start").addEventListener("click", init);
+// document.getElementById('name').addEventListener('change', onChangeName);
 
-//step 2 AND 3
+// step 2 AND 3
 
 var y = 0;
 var x = 0;
-var SPEED = 1;
+var SPEED = 10;
 
+// TODO : remove hard-coded values (ex: 450) and use player's dimensions to calculate the new position
 var move = function(e) {
   if (e.key === "ArrowUp") {
     // up arrow
@@ -35,7 +51,7 @@ var move = function(e) {
     }
   } else if (e.key === "ArrowDown") {
     // down arrow fonctionne
-    if (y !== 440) {
+    if (y <= 450) {
       y += SPEED;
       document.getElementsByClassName("disk")[0].style.marginTop = `${y}px`;
     }
@@ -47,7 +63,7 @@ var move = function(e) {
     }
   } else if (e.key === "ArrowRight") {
     // right arrow fonctionne
-    if (x !== 740) {
+    if (x !== 750) {
       x += SPEED;
       document.getElementsByClassName("disk")[0].style.marginLeft = `${x}px`;
     }
@@ -55,3 +71,15 @@ var move = function(e) {
 };
 
 window.addEventListener("keydown", move);
+
+// STEP 4 : initialize the map with all the circles (cf TODOs above)
+
+// STEP 5 : console.log when you the player hits a litte circle
+
+// STEP 6 : remove the circle from the playzone when the player hits it
+
+// STEP 7 : enlarge the player when it hits a circle taking the circle's size into account
+
+// STEP 8 : handle a continous movement of the player
+// If you press an arrow key, the player keeps moving in the desired direction
+// until another arrow key is pressed or it hits a wall
